d6c16084cdec4e8567ffd6b1e138e6bac294b385
nosniboD <michael.dobinson@gmail.com>
2023-03-30 10:47:52 +0100

    add server content

--- listen.js
+++ listen.js
@@ -1,6 +1,8 @@
 const app = require('./app');
 
+const { PORT = 9090 } = process.env;
+
 app.listen(9090, (err) => {
   if (err) console.log(err);
-  else console.log('listening on port 9090');
+  else console.log(`listening on port ${PORT}`);
 });
